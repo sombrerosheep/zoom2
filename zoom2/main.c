8566a828fe796f3c9d9006cd882eabe39a12a6b2
sombrerosheep <sombrerosheep@gmail.com>
2020-01-25 17:03:52 -0600

    using manual gl context

--- zoom2/main.c
+++ zoom2/main.c
@@ -1,14 +1,165 @@
 #include <Windows.h>
-#include <tchar.h>
 #include <stdio.h>
 
-static TCHAR APP_NAME[] = _T("vroom 2 :: the sequel");
+#include <gl/GL.h>
+#include <glext.h>
+#include <wglext.h>
+
+static const char *app_name = "vroom 2 :: the sequel";
 static const int HEIGHT = 600;
 static const int WIDTH = 800;
 
 LRESULT CALLBACK
+WindowProc(HWND window, UINT message, WPARAM wParam, LPARAM lParam);
+
+typedef struct RenderContext {
+  HDC device;
+  HGLRC glContext;
+} RenderContext;
+
+RenderContext renderer;
+PFNWGLCREATECONTEXTATTRIBSARBPROC wglCreateContextAttribsARB;
+PFNWGLCHOOSEPIXELFORMATARBPROC wglChoosePixelFormatARB;
+
+void load_gl_functions() {
+  wglCreateContextAttribsARB = wglGetProcAddress("wglCreateContextAttribsARB");
+  wglChoosePixelFormatARB = wglGetProcAddress("wglChoosePixelFormatARB");
+}
+
+HWND make_window(RenderContext *context) {
+  PIXELFORMATDESCRIPTOR fakePfd = {
+    .nSize = sizeof(PIXELFORMATDESCRIPTOR),
+    .nVersion = 1,
+    .dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,
+    .iPixelType = PFD_TYPE_RGBA,
+    .cColorBits = 32,
+    .cDepthBits = 24,
+    .cStencilBits = 8,
+  };
+
+  WNDCLASSEX wndClass = {
+    .cbSize = sizeof(WNDCLASSEX),
+    .style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC,
+    .lpfnWndProc = WindowProc,
+    .cbClsExtra = 0,
+    .cbWndExtra = 0,
+    .hInstance = 0,
+    .hIcon = LoadIcon(0, IDI_APPLICATION),
+    .hCursor = LoadCursor(NULL, IDC_ARROW),
+    .hbrBackground = (HBRUSH)(COLOR_WINDOW + 1),
+    .lpszMenuName = NULL,
+    .lpszClassName = app_name,
+    .hIconSm = LoadIcon(0, IDI_APPLICATION)
+  };
+
+  if (!RegisterClassEx(&wndClass)) {
+    MessageBox(NULL, "Call to RegisterClassEx failed!", app_name, MB_OK);
+    return NULL;
+  }
+
+  HWND fakeWnd = CreateWindow(
+    app_name, "fake window",
+    WS_CLIPSIBLINGS | WS_CLIPCHILDREN,
+    0, 0, 1, 1,
+    NULL, NULL, 0, NULL
+  );
+  if (fakeWnd == NULL) {
+    MessageBox(NULL, "Error init fake window", app_name, MB_OK);
+    return NULL;
+  }
+
+  HDC fake_context = GetDC(fakeWnd);
+  int fakePFDID = ChoosePixelFormat(fake_context, &fakePfd);
+  if (fakePFDID == 0) {
+    MessageBox(NULL, "Failure calling ChoosePixelFormat", app_name, MB_OK);
+    return NULL;
+  }
+
+  if (!SetPixelFormat(fake_context, fakePFDID, &fakePfd)) {
+    MessageBox(NULL, "Failure calling SetPixelFormat", app_name, MB_OK);
+    return NULL;
+  }
+
+  HGLRC fake_glc = wglCreateContext(fake_context);
+  if (fake_glc == 0) {
+    MessageBox(NULL, "Failure creating fgl context", app_name, MB_OK);
+    return NULL;
+  }
+
+  if (!wglMakeCurrent(fake_context, fake_glc)) {
+    MessageBox(NULL, "Failure making fctx current", app_name, MB_OK);
+    return NULL;
+  }
+
+  load_gl_functions();
+
+  HWND window = CreateWindowEx(
+    WS_EX_APPWINDOW,
+    app_name, app_name,
+    WS_OVERLAPPEDWINDOW,
+    CW_USEDEFAULT, CW_USEDEFAULT,
+    WIDTH, HEIGHT,
+    NULL, NULL, 0, NULL
+  );
+  context->device = GetDC(window);
+  if (context->device == NULL) {
+    MessageBox(NULL, "failed to get device context", app_name, MB_OK);
+  }
+
+  const int attribs[] = {
+    WGL_DRAW_TO_WINDOW_ARB, GL_TRUE,
+    WGL_SUPPORT_OPENGL_ARB, GL_TRUE,
+    WGL_DOUBLE_BUFFER_ARB, GL_TRUE,
+    WGL_PIXEL_TYPE_ARB, WGL_TYPE_RGBA_ARB,
+    WGL_ACCELERATION_ARB, WGL_FULL_ACCELERATION_ARB,
+    WGL_COLOR_BITS_ARB, 32,
+    WGL_ALPHA_BITS_ARB, 8,
+    WGL_DEPTH_BITS_ARB, 24,
+    WGL_STENCIL_BITS_ARB, 8,
+    WGL_SAMPLE_BUFFERS_ARB, GL_TRUE,
+    WGL_SAMPLES_ARB, 4,
+    0
+  };
+
+  int formatId;
+  UINT numFormats;
+  BOOL status = wglChoosePixelFormatARB(context->device, attribs, NULL, 1, &formatId, &numFormats);
+  if (status == FALSE || numFormats == 0) {
+    MessageBox(NULL, "Could not get a pixel format", app_name, MB_OK);
+    return NULL;
+  }
+
+  PIXELFORMATDESCRIPTOR pfd;
+  DescribePixelFormat(context->device, formatId, sizeof(pfd), &pfd);
+  SetPixelFormat(context->device, formatId, &pfd);
+
+  int contextAttribs[] = {
+    WGL_CONTEXT_MAJOR_VERSION_ARB, 3,
+    WGL_CONTEXT_MINOR_VERSION_ARB, 3,
+    WGL_CONTEXT_PROFILE_MASK_ARB, WGL_CONTEXT_CORE_PROFILE_BIT_ARB,
+    0
+  };
+  context->glContext = wglCreateContextAttribsARB(context->device, 0, contextAttribs);
+  if (context->glContext == NULL) {
+    MessageBox(NULL, "could not create gl context", app_name, MB_OK);
+    return NULL;
+  }
+
+  wglMakeCurrent(NULL, NULL);
+  wglDeleteContext(fake_glc);
+  ReleaseDC(fake_context, fakeWnd);
+  DestroyWindow(fakeWnd);
+  if (!wglMakeCurrent(context->device, context->glContext)) {
+    MessageBox(NULL, "failed to set final context", app_name, MB_OK);
+    return NULL;
+  }
+
+  return window;
+}
+
+LRESULT CALLBACK
 WindowProc(
-  HWND hWnd,
+  HWND window,
   UINT message,
   WPARAM wParam,
   LPARAM lParam
@@ -17,22 +168,21 @@ WindowProc(
   HDC hdc;
 
   switch (message) {
-    case WM_DESTROY: {
+    case WM_CLOSE: {
       PostQuitMessage(0);
       break;
     }
     case WM_PAINT: {
-      hdc = BeginPaint(hWnd, &ps);
+      hdc = BeginPaint(window, &ps);
 
-      TCHAR hello[] = _T("hello world");
-      TextOut(hdc, 5, 5, hello, _tcslen(hello));
+      char *ver = glGetString(GL_VERSION);
+      TextOut(hdc, 5, 5, ver, strlen(ver));
 
-      EndPaint(hWnd, &ps);
+      EndPaint(window, &ps);
       break;
     }
     default: {
-      return DefWindowProc(hWnd, message, wParam, lParam);
-      break;
+      return DefWindowProc(window, message, wParam, lParam);
     }
   }
 
@@ -41,62 +191,43 @@ WindowProc(
 
 int CALLBACK
 WinMain(
-  _In_ HINSTANCE hInstance,
-  _In_opt_ HINSTANCE hPrevInstance,
-  _In_ LPSTR     lpCmdLine,
-  _In_ int       nCmdShow
+  HINSTANCE hInstance,
+  HINSTANCE hPrevInstance,
+  LPSTR     lpCmdLine,
+  int       nCmdShow
 ) {
   printf("Hello world\n");
 
-  WNDCLASSEX wndClass = {
-    .cbSize = sizeof(WNDCLASSEX),
-    .style = CS_HREDRAW | CS_VREDRAW,
-    .lpfnWndProc = WindowProc,
-    .cbClsExtra = 0,
-    .cbWndExtra = 0,
-    .hInstance = hInstance,
-    .hIcon = LoadIcon(hInstance, IDI_APPLICATION),
-    .hCursor = LoadCursor(NULL, IDC_ARROW),
-    .hbrBackground = (HBRUSH)(COLOR_WINDOW + 1),
-    .lpszMenuName = NULL,
-    .lpszClassName = APP_NAME,
-    .hIconSm = LoadIcon(hInstance, IDI_APPLICATION)
+  // create window
+  RenderContext ctx = {
+    .device = NULL,
+    .glContext = NULL
   };
+  HWND window = make_window(&ctx);
 
-  if (!RegisterClassEx(&wndClass)) {
-    MessageBox(NULL, "Call to RegisterClassEx failed!", APP_NAME, NULL);
-
+  if (!window) {
+    MessageBox(NULL, "Call to CreateWindow failed", app_name, MB_OK);
     return -1;
   }
-
-  HWND hWnd = CreateWindowEx(
-    WS_EX_APPWINDOW,
-    wndClass.lpszClassName,
-    APP_NAME,
-    WS_OVERLAPPEDWINDOW,
-    CW_USEDEFAULT,
-    CW_USEDEFAULT,
-    WIDTH,
-    HEIGHT,
-    NULL,
-    NULL,
-    hInstance,
-    NULL
-  );
-
-  if (!hWnd) {
-    MessageBox(NULL, "Call to CreateWindow failed", APP_NAME, NULL);
-
-    return -1;
-  }
-  ShowWindow(hWnd, nCmdShow);
-  UpdateWindow(hWnd);
-
+  
+  ShowWindow(window, nCmdShow);
+  BOOL running = TRUE;
   MSG msg;
-  while (GetMessage(&msg, NULL, 0, 0)) {
-    TranslateMessage(&msg);
-    DispatchMessage(&msg);
+  while (running == TRUE) {
+    while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+      if (msg.message == WM_QUIT) {
+        running = FALSE;
+      }
+
+      TranslateMessage(&msg);
+      DispatchMessage(&msg);
+    }
   }
+
+  wglMakeCurrent(NULL, NULL);
+  wglDeleteContext(ctx.glContext);
+  ReleaseDC(window, ctx.device);
+  DestroyWindow(window);
 
   return (int)msg.wParam;
 }
